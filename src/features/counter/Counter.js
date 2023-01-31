a5a466c33c4cd8f4d943b4520206070bc32bff5e
newman-afk <84593706+newman-afk@users.noreply.github.com>
2023-01-31 18:40:57 +0800

    1

---
+++ src/features/counter/Counter.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { decrement, increment, incrementByAmount } from "./counterSlice";
+
+export function Counter() {
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
+
+  return (
+    <>
+      <span>{count}</span>
+      <div>
+        <button
+          aria-label="Increment value"
+          onClick={() => dispatch(increment())}
+        >
+          Increment
+        </button>
+
+        <button
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement
+        </button>
+        <button onClick={() => dispatch(incrementByAmount(5))}>
+          incrementByAmount
+        </button>
+      </div>
+    </>
+  );
+}
